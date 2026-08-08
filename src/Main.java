24919fa182ec6bef1f49f37f7890408057ca232a
Guilherme Amorim <ggmamorim@gmail.com>
2026-08-08 11:52:00 -0300

    Criei a classe ContaPoupanca
    Criei a classe ContaPoupanca com um atributo novo (juros) e um novo método (renderJuros).

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 import dados.ContaCorrente;
+import dados.ContaPoupanca;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         // classe usada para testes até o presente momento.
-        ContaCorrente c1 = new ContaCorrente("Guilherme");
-        System.out.println(c1);
-        System.out.println(c1.getSaldo());
-        System.out.println(c1.depositar(0));
-        System.out.println(c1.depositar(500));
-        System.out.println(c1.sacar(0));
-        System.out.println(c1.sacar(1000));
-        System.out.println(c1.sacar(300));
+        ContaPoupanca p1 = new ContaPoupanca("Guilherme");
+        System.out.println(p1);
+        p1.renderJuros();
+        System.out.println(p1);
     }
 }
